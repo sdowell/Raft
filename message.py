68810b2a1c2aff9c7c261bc70ba9a106f8b17fac
sdowell <sdowell@umail.ucsb.edu>
2017-03-08 09:35:06 -0800

    Create Raft message structure

--- message.py
+++ message.py
@@ -1,4 +1,5 @@
 import struct
+import pickle
 from math import floor, ceil
 BUY_MESSAGE_CODE = 1
 BUY_MESSAGE_RESPONSE_CODE = 2
@@ -19,103 +20,80 @@ class Message:
 
 	@staticmethod
 	def deserialize(data):
-		message_type = struct.unpack("!B",data[0:1])[0]
-		if message_type == BUY_MESSAGE_CODE:
-			return BuyMessage.deserialize(data)
-		elif message_type == BUY_MESSAGE_RESPONSE_CODE:
-			return BuyMessageResponse.deserialize(data)
-		elif message_type == REQUEST_MESSAGE_CODE:
-			return RequestMessage.deserialize(data)
-		elif message_type == REPLY_MESSAGE_CODE:
-			return ReplyMessage.deserialize(data)
-		elif message_type == RELEASE_MESSAGE_CODE:
-			return ReleaseMessage.deserialize(data)
-		else: #other message types
-			pass
-
-class ReleaseMessage(Message):
-	
-	def serialize(self):
-		return struct.pack("!BI", RELEASE_MESSAGE_CODE, self.num_tickets)
-
-	def __init__(self, num_tickets):
-		self.num_tickets = num_tickets
-		super(ReleaseMessage, self).__init__(self.serialize())
+		return pickle.loads(data)
 
 	@staticmethod
-	def deserialize(data):
-		msg_code, num_tickets = struct.unpack("!BI", data)
-		assert msg_code == RELEASE_MESSAGE_CODE
-		return ReleaseMessage(num_tickets)
-	
-class ReplyMessage(Message):
-
-	def serialize(self):
-		return struct.pack("!BI", REPLY_MESSAGE_CODE, 0)
-
-	def __init__(self):
-		super(ReplyMessage, self).__init__(self.serialize())
-
-	@staticmethod	
-	def deserialize(data):
-		msg_code, fpoint = struct.unpack("!BI", data)
-		assert msg_code == REPLY_MESSAGE_CODE
-		return ReplyMessage()
-
-def get_rank(lclock, pid):
-	return (float(lclock) + (float(pid)/TOTAL_KIOSKS))
-
-class RequestMessage(Message):
-	
-	def serialize(self):
-		return struct.pack("!Bf", REQUEST_MESSAGE_CODE, (float(self.lamport_clock) + float(self.pid)/TOTAL_KIOSKS))
-
-	def __init__(self, lamport_clock, process_id):
-		self.lamport_clock = lamport_clock
-		self.pid = process_id
-		self.rank = get_rank(self.lamport_clock, self.pid)
-		super(RequestMessage, self).__init__(self.serialize())
-	
-	@staticmethod
-	def deserialize(data):
-		msg_code, fpoint = struct.unpack("!Bf", data)
-		assert msg_code == REQUEST_MESSAGE_CODE
-		lamport_clock = floor(fpoint)
-		process_id = (float(fpoint)-float(lamport_clock))*float(TOTAL_KIOSKS)
-		process_id = int(round(process_id))
-		return RequestMessage(lamport_clock, process_id)
+	def serialize(data):
+		return pickle.dumps(self)
 		
-	
-
-class BuyMessage(Message):
+class ClientBuyRequest(Message):
 
 	def serialize(self):
-		return struct.pack("!BI", BUY_MESSAGE_CODE, self.num_tickets)
+		return pickle.dumps(self)
 
 	def __init__(self, num_tickets):
 		self.num_tickets = num_tickets
-		super(BuyMessage, self).__init__(self.serialize())
-
-	@staticmethod
-	def deserialize(data):
-		msg_code, num_tickets = struct.unpack("!BI",data[0:5])
-		assert msg_code == BUY_MESSAGE_CODE
-		return BuyMessage(int(num_tickets))
-
-class BuyMessageResponse(Message):
-
+		super(ClientBuyRequest, self).__init__(self.serialize())
+		
+class ClientBuyResponse(Message):
 	def serialize(self):
-		return struct.pack("!BI", BUY_MESSAGE_RESPONSE_CODE, self.success)
+		return pickle.dumps(self)
 
-	def __init__(self, successful):
-		if successful is True:
-			self.success = BUY_SUCCESS
-		else:
-			self.success = BUY_FAIL
-		super(BuyMessageResponse, self).__init__(self.serialize())
-	
-	@staticmethod
-	def deserialize(data):
-		msg_code, success = struct.unpack("!BI", data)
-		assert msg_code == BUY_MESSAGE_RESPONSE_CODE
-		return BuyMessageResponse(success == BUY_SUCCESS)
+	def __init__(self, num_tickets, success, leader=None):
+		self.num_tickets = num_tickets
+		self.success = success
+		self.leader = leader
+		super(ClientBuyResponse, self).__init__(self.serialize())
+
+class ClientConfigRequest(Message):
+	def serialize(self):
+		return pickle.dumps(self)
+
+	def __init__(self, num_tickets, success, leader=None):
+		self.num_tickets = num_tickets
+		self.success = success
+		self.leader = leader
+		super(ClientConfigRequest, self).__init__(self.serialize())
+		
+class RequestVote(Message):
+	def serialize(self):
+		return pickle.dumps(self)
+
+	def __init__(self, cand_id, term, log_index, log_term):
+		self.cand_id = cand_id
+		self.term = term
+		self.log_index = log_index
+		self.log_term = log_term
+		super(RequestVote, self).__init__(self.serialize())
+		
+class RequestVoteResponse(Message):
+	def serialize(self):
+		return pickle.dumps(self)
+
+	def __init__(self, voteGranted, term):
+		self.voteGranted = voteGranted
+		self.term = term
+		super(RequestVoteResponse, self).__init__(self.serialize())
+		
+class AppendEntries(Message):
+	def serialize(self):
+		return pickle.dumps(self)
+
+	def __init__(self, term, leader_id, prevLogIndex, prevLogTerm, entries, commitIndex):
+		self.term = term
+		self.leader_id
+		self.prevLogIndex = prevLogIndex
+		self.prevLogTerm = prevLogTerm
+		self.entries = entries
+		self.commitIndex = commitIndex
+		super(AppendEntries, self).__init__(self.serialize())
+		
+class AppendEntriesResponse(Message):
+	def serialize(self):
+		return pickle.dumps(self)
+
+	def __init__(self, num_tickets, success, leader=None):
+		self.num_tickets = num_tickets
+		self.success = success
+		self.leader = leader
+		super(AppendEntriesResponse, self).__init__(self.serialize())
